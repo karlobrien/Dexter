8ed8f4fb08655bd5688a5e557d96734f42eacdde
Karl O'Brien <karlobrien@gmail.com>
2018-11-22 16:32:48 +0000

    project setup

---
+++ src/Dexter.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dexter.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
