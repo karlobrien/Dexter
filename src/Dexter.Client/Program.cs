a726f49809aef0c28554c5087c13044c2ad8c4b3
Karl O'Brien <karlobrien@gmail.com>
2018-11-24 13:26:16 +0000

    wcf example

--- src/Dexter.Client/Program.cs
+++ src/Dexter.Client/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using NetMQ;
+using NetMQ.Sockets;
 
 namespace Dexter.Client
 {
@@ -7,6 +9,20 @@ namespace Dexter.Client
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            using (var client = new RequestSocket())
+            {
+                client.Connect("tcp://localhost:5555");
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Sending Hello");
+                    client.SendFrame("Hello");
+
+                    var message = client.ReceiveFrameString();
+                    Console.WriteLine("Received {0}", message);
+                }
+            }
         }
     }
 }
--- src/Dexter.Server/Program.cs
+++ src/Dexter.Server/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NetMQ;
+using NetMQ.Sockets;
 
 namespace Dexter.Server
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Task serverTask = Task.Run(() => Server());
+            Console.WriteLine("Server");
+            Console.ReadLine();
+        }
+
+        public static void Server()
+        {
+            using (var server = new ResponseSocket())
+            {
+                server.Bind("tcp://*:5555");
+
+                while (true)
+                {
+                    var message = server.ReceiveFrameString();
+                    Console.WriteLine("Received {0}", message);
+                    Console.WriteLine("Sending World");
+                    server.SendFrame("World");
+
+                    if (message == "exit")
+                        break;
+                }
+            }
         }
     }
+
+
 }
