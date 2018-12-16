ae1dbf7c2743f3266a5af78eef3c21c47b6462ec
Karl O'Brien <karlobrien@gmail.com>
2018-12-16 12:32:58 +0000

    Move to a poller implementation for client

--- src/Dexter.Client/MulticastSubscriber.cs
+++ src/Dexter.Client/MulticastSubscriber.cs
@@ -30,14 +30,6 @@ namespace Dexter.Client
                     var unwrap = MarketData.Parser.ParseFrom(message.Last.Buffer);
                     Console.WriteLine(unwrap.Instrument);
                 }
-
-                /* var mdTopicReceived = _subscriberSocket.TryReceiveFrameString(out var messageTopicReceived);
-                if (mdTopicReceived)
-                {
-                    var mdMsgRecd = _subscriberSocket.TryReceiveFrameBytes(out var msg);
-                    var messageReceived = MarketData.Parser.ParseFrom(msg);
-                    Console.WriteLine($"PubSub: topic: {messageTopicReceived}, msg: {messageReceived}");
-                } */
             }
         }
     }
